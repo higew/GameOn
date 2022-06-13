e444c94f4cb8f266142cb8b14992a01d2b430d4c
Brice Meyer <hi.meyerb@gmail.com>
2022-06-13 11:18:13 +0200

    Added regex validation for the email

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -14,6 +14,7 @@ const formData = document.querySelectorAll(".formData");
 const modalClose = document.querySelectorAll(".close");
 const firstName = document.getElementById('first');
 const lastName = document.getElementById('last');
+const email = document.getElementById('email');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -36,6 +37,7 @@ function closeModal (){
 
 firstName.addEventListener('input', isFirstNameValid);
 lastName.addEventListener('input', isLastNameValid);
+email.addEventListener('input', isEmailValid);
 
 // function check input
 
@@ -50,7 +52,12 @@ function hideError(el) {
 function isFirstNameValid () {
   const parent = firstName.closest('div');
   showError(parent);
+
+  //Old regex ghetto
+
   // if (firstName.value.trim().length < 2 || firstName.value.trim() === "" || !/^[A-zÀ-ÿ][A-zÀ-ÿ-][A-zÀ-ÿ]+$/.test(firstName.value)) {
+
+  //Regex is Allowing Accent and also hyphen between names but not before and after names
   if (firstName.value.trim().length < 2 || firstName.value.trim() === "" || !/^[A-zÀ-ÿ]+(?:-?[A-zÀ-ÿ]+)+$/.test(firstName.value)) {
     return false;
   } 
@@ -65,6 +72,17 @@ function isLastNameValid () {
   if (lastName.value.trim().length < 2 || lastName.value.trim() === "" || !/^[A-zÀ-ÿ]+(?:-?[A-zÀ-ÿ]+)+$/.test(lastName.value)) {
     return false;
   } 
+
+  hideError(parent);
+  return true;
+}
+
+function isEmailValid () {
+  const parent = email.closest('div');
+  showError(parent);
+  if (!/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/.test(email.value)) {
+    return false;
+  }
 
   hideError(parent);
   return true;
