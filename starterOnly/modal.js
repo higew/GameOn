10b31cae5e569375c04322cd6bae388e13334a35
Brice Meyer <hi.meyerb@gmail.com>
2022-06-14 12:00:02 +0200

    Added regex validation for birthdate

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -15,6 +15,7 @@ const modalClose = document.querySelectorAll(".close");
 const firstName = document.getElementById('first');
 const lastName = document.getElementById('last');
 const email = document.getElementById('email');
+const birthdate = document.getElementById('birthdate');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -38,6 +39,7 @@ function closeModal (){
 firstName.addEventListener('input', isFirstNameValid);
 lastName.addEventListener('input', isLastNameValid);
 email.addEventListener('input', isEmailValid);
+birthdate.addEventListener('input', isBirthdateValid);
 
 // function check input
 
@@ -57,7 +59,7 @@ function isFirstNameValid () {
 
   // if (firstName.value.trim().length < 2 || firstName.value.trim() === "" || !/^[A-zÀ-ÿ][A-zÀ-ÿ-][A-zÀ-ÿ]+$/.test(firstName.value)) {
 
-  //Regex is Allowing Accent and also hyphen between names but not before and after names
+  //Regex is Allowing Accent and also hyphen between names but not before and after names and also deny double hyphen
   if (firstName.value.trim().length < 2 || firstName.value.trim() === "" || !/^[A-zÀ-ÿ]+(?:-?[A-zÀ-ÿ]+)+$/.test(firstName.value)) {
     return false;
   } 
@@ -69,6 +71,8 @@ function isFirstNameValid () {
 function isLastNameValid () {
   const parent = lastName.closest('div');
   showError(parent);
+
+  //same as above
   if (lastName.value.trim().length < 2 || lastName.value.trim() === "" || !/^[A-zÀ-ÿ]+(?:-?[A-zÀ-ÿ]+)+$/.test(lastName.value)) {
     return false;
   } 
@@ -80,7 +84,22 @@ function isLastNameValid () {
 function isEmailValid () {
   const parent = email.closest('div');
   showError(parent);
+
+  //verify that only letter, number and (underscore or hyphen) are written before the @ and same for the hosting name after the @ + verify that only 2-5 letter only after the dot
   if (!/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/.test(email.value)) {
+    return false;
+  }
+
+  hideError(parent);
+  return true;
+}
+
+function isBirthdateValid () {
+  const parent = birthdate.closest('div');
+  showError(parent);
+
+  //verify date input value to be formated like YYYY-MM-DD (HTML input type date format)
+  if (!/^\d{4}(\-)(((0)[1-9])|((1)[0-2]))(\-)([0-2][0-9]|(3)[0-1])$/.test(birthdate.value)) {
     return false;
   }
 
